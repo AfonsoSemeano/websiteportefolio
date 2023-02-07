b69128c705cd4c0c33186315bf408c0dc302dace
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2023-02-07 17:06:03 +0000

    new styling brief

--- src/portfoliowebsite.jsx
+++ src/portfoliowebsite.jsx
@@ -8,24 +8,25 @@ function PortfolioWebsite(props) {
     }
 
     return (
-        <div className="content ms-4 mt-3 pb-3">
+        <div className="content ps-4 pt-3 pb-3 bg-secondary">
             <h1>{translate("title")}</h1>
-            <p className="fw-bold fs-4">{translate("reason")}</p> 
-            <p>{translate("reasondesc")}</p>
-            <p className="fw-bold fs-4">{translate("howcreated")}</p>
-            <p>{translate("howcreateddesc")}</p>
-            <p className="fw-bold fs-4">{translate("difficulties")}</p>
-            <p>{translate("difficultiesdesc")}</p>
-            <ul>
-                <li>{translate("diffresponsiveness")}</li>
-                <li>{translate("diffloading")}</li>
-                <li>{translate("difflanguage")}</li>
-                <li>{translate("difflogin")}</li>
-                <li>{translate("diffbeautiful")}</li>
-            </ul>
-            <p className="fw-bold fs-4">{translate("seecode")}</p>
-            <p>{translate("seecodedesc")} <a href="https://github.com/AfonsoSemeano/websiteportefolio">link</a></p>
-
+            <div className="ms-5">
+                <p className="fw-bold fs-4">{translate("reason")}</p> 
+                <p>{translate("reasondesc")}</p>
+                <p className="fw-bold fs-4">{translate("howcreated")}</p>
+                <p>{translate("howcreateddesc")}</p>
+                <p className="fw-bold fs-4">{translate("difficulties")}</p>
+                <p>{translate("difficultiesdesc")}</p>
+                <ul>
+                    <li>{translate("diffresponsiveness")}</li>
+                    <li>{translate("diffloading")}</li>
+                    <li>{translate("difflanguage")}</li>
+                    <li>{translate("difflogin")}</li>
+                    <li>{translate("diffbeautiful")}</li>
+                </ul>
+                <p className="fw-bold fs-4">{translate("seecode")}</p>
+                <p>{translate("seecodedesc")} <a href="https://github.com/AfonsoSemeano/websiteportefolio">link</a></p>
+            </div>
         </div>
     );
 }
