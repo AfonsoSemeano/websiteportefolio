ebe789ef95c766baa97d66930d7d9c3a4eda1230
AfonsoSemeano <80034415+AfonsoSemeano@users.noreply.github.com>
2023-04-12 13:03:13 +0100

    added a report and fixed linkedin link

--- src/footer.js
+++ src/footer.js
@@ -19,7 +19,7 @@ function Footer(props) {
                     <Col className='d-flex align-items-center flex-column d-md-block'>
                         <h5 className='fw-semibold navbar-text-color'>{translate("getintouch")}:</h5>
                         <p className='navbar-text-color'>Email: afonsosemeano@gmail.com</p>
-                        <p className='navbar-text-color'>Linkedin: none</p>
+                        <p className='navbar-text-color'>Linkedin: <a className='text-white' href='https://www.linkedin.com/in/afonso-semeano-a6699a268/'>Afonso Semeano</a></p>
                     </Col>
                 </Row>
             </Container>
